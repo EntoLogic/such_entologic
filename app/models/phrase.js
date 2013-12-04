56cff4d353ce54eb0f535b497d9537de11a8efc1
Rory Hughes <rory@roryhughes.net>
2013-12-04 20:59:23 +0000

    actually adding it now

---
+++ app/models/phrase.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose'),
+    config = require('../../config/config'),
+    Schema = mongoose.Schema,
+    timestamps = require('mongoose-timestamp');
+
+// TODO Require validations (and others)
+
+var ClauseSchema = new Schema({
+  words: [String],
+  flags: [{type: Schema.Types.ObjectId, ref: 'User'}],
+  condition: {
+    conditionType: String,
+    reverse: Boolean,
+    nodeAttribute: String,
+    comparator: String,
+    comparedWith: Number
+  }
+});
+
+// http://mongoosejs.com/docs/subdocs.html
+
+var PhraseSchema = new Schema({
+  nodeName: String,
+  pLang: String,
+  nLang: String,
+  inUse: Boolean,
+  voteCache: {type: Number, default: 0},
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  forkedFrom: {
+    type: Schema.Types.ObjectId,
+    ref: 'Phrase'
+  },
+  clauses: [ClauseSchema]
+});
+
+PhraseSchema.plugin(timestamps);
+
+/**
+ * Validations
+ */
+ClauseSchema.path('words').validate(function(words) {
+  return words.length;
+}, 'There must be at least one clause.');
+
+
+
+// *
+//  * Statics
+ 
+// ArticleSchema.statics = {
+//   load: function(id, cb) {
+//     this.findOne({
+//       _id: id
+//     }).populate('user', 'name username').exec(cb);
+//   }
+// };
+
+mongoose.model('Phrase', PhraseSchema);
